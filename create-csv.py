0159db679d862a3a5598158d2890a2271778d940
er-knight <er-knight@protonmail.com>
2021-10-25 19:54:11 +0530

    update create-csv.py, tasks notebook and dataset

--- create-csv.py
+++ create-csv.py
@@ -3,28 +3,21 @@ import pandas as pd
 
 def create_csv(excel_file_path: str, country_name: str) -> str:
 
-    required_series_names = [
-        "Population growth (annual %)",
-        "CO2 emissions (metric tons per capita)",
-        "GDP growth (annual %)",  
-        "Inflation, GDP deflator (annual %)",  
-        "Industry (including construction), value added (% of GDP)",  
-        "Exports of goods and services (% of GDP)",  
-        "Imports of goods and services (% of GDP)",  
-        "Gross capital formation (% of GDP)",  
-        "Inflation, consumer prices (annual %)"
-    ]
+    excel_data = pd.read_excel(excel_file_path, sheet_name="Data", na_values=[".."])
 
+    series_names    = excel_data["Series Name"].unique() 
     columns_to_drop = ["Series Name", "Series Code", "Country Name", "Country Code"]
 
-    excel_data    = pd.read_excel(excel_file_path, sheet_name="Data", na_values=[".."])
     country_data  = excel_data[excel_data["Country Name"] == country_name]
     required_data = {"Years" : [year for year in range(1990, 2021)]}
 
-    for series_name in required_series_names:
-        required_data[series_name] = country_data[
-            country_data["Series Name"] == series_name
-        ].drop(columns_to_drop, axis=1).to_numpy().tolist()[0]
+    for series_name in series_names:
+        try:
+            required_data[series_name] = country_data[
+                country_data["Series Name"] == series_name
+            ].drop(columns_to_drop, axis=1).to_numpy().tolist()[0]
+        except:
+            pass 
 
     country_name = "-".join([name.lower() for name in country_name.split()])
 
